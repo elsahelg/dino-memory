9216347cbd81854504876b8cfce1a194a5f0d997
Elsa Helg <elsahelg@gmail.com>
2022-11-26 00:21:06 +0000

    Add main structure to script

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+ //The game board 
+ function shuffleCards()
+
+ //Flip cards
+ function flipCards()
+ 
+ //check for match
+ function checkMatch ()
+
+ //attempts count
+ function incrementAttempts ()
+
